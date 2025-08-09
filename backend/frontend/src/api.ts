d09fbb35b3b76cf3da306c4cb9a2e2cb0eb57b0a
Daniel031722 <daniel031722@gmail.com>
2025-08-09 15:24:57 +0530

    Removed

--- backend/frontend/src/api.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000", // backend service URL
-  withCredentials: true
-});
-
-export default api;
